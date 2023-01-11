c138f9cd7977d3b5bca9ca9a3ae56260d9fb9164
Nur Hidayat <nur.hidayat454.nh@gmail.com>
2023-01-11 22:27:44 +0700

    Update Library

--- core/src/main/java/com/nurhidayaatt/core/data/source/remote/response/movie/ListMovieResponse.kt
+++ core/src/main/java/com/nurhidayaatt/core/data/source/remote/response/movie/ListMovieResponse.kt
@@ -4,5 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class ListMovieResponse(
     @SerializedName("results")
-    val results: ArrayList<MovieResponse>
+    val results: List<MovieResponse>
 )
